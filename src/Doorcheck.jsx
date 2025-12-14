652aa788cc228d05dd94a836e375f7c5202d5ae5
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2025-12-14 11:26:22 +0200

    Creating a separate state variable for a different door
    If the same state variable is used for 2 elements, the data will be shared to both concurrently.

--- src/Doorcheck.jsx
+++ src/Doorcheck.jsx
@@ -1,17 +1,29 @@
 import {useState} from "react";
 
 export default function doorCheck(){
-    const [doorStatus, setdoorStatus] = useState("Closed");
+    const [doorStatus1, setdoorStatus1] = useState("Closed");
+    // If you dont make a separate variable, both elements are affected at once
+    const [doorStatus2, setdoorStatus2] = useState("Closed");
 
-    function changeDoor() {
-        setdoorStatus(prev =>
+    function changeDoor1() {
+        setdoorStatus1(prev =>
             prev === "Closed" ? "Opened" : "Closed"
         );
         
     }
 
+    function changeDoor2() {
+        setdoorStatus2(prev =>
+            prev === "Closed" ? "Opened" : "Closed"
+        );
+    }
+
 
     return (
-        <button onClick={changeDoor}>Door {doorStatus}</button>
+        <>
+            <button onClick={changeDoor1}>Door 1 {doorStatus1}</button>
+            <button onClick={changeDoor2}>Door 2 {doorStatus2}</button>
+        </>
+
     );
 }
